81b265d80f5d90b3926347f060f6132c5f9f5141
minosse99 <simoneboldrini1999@gmail.com>
2022-10-14 18:27:01 +0200

    add location

--- app/src/components/Location.tsx
+++ app/src/components/Location.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text ,Alert} from 'react-native';
+
+import Geolocation from '@react-native-community/geolocation';
+
 
 const Location = () => {
+
+    const [
+        currentLongitude,
+        setCurrentLongitude
+      ] = useState(Number);
+      const [
+        currentLatitude,
+        setCurrentLatitude
+      ] = useState(Number);
+     
+    Geolocation.getCurrentPosition(
+        //Will give you the current location
+        (position) => {
+        
+        //Setting Longitude state
+        setCurrentLongitude(position.coords.longitude);
+        
+        //Setting Longitude state
+        setCurrentLatitude(position.coords.latitude);
+            }, (error) => Alert.alert(error.message), { 
+            enableHighAccuracy: false,timeout :200 ,maximumAge: 1000 
+            //if we enableHighaccuracy the time of response might be longer than false & increased power consumption
+            }
+        );
+
     return (
         <View>
-            <Text>This is the location screen </Text>
+            <Text>Position: Long:{currentLongitude} Lat:{currentLatitude} </Text>
         </View>
     );
+
 }
 export default Location;
