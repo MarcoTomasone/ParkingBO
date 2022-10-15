e9d08874a40ff31e42915fd0c56d93bf8955b58e
minosse99 <simoneboldrini1999@gmail.com>
2022-10-15 12:30:29 +0200

    add location

--- app/src/components/Location.tsx
+++ app/src/components/Location.tsx
@@ -1,40 +1,35 @@
-import React, {useState} from 'react';
-import { View, Text ,Alert} from 'react-native';
+import React, {useState,useEffect} from 'react';
+import { View, Text} from 'react-native';
+
 
 import Geolocation from '@react-native-community/geolocation';
 
 
 const Location = () => {
-
-    const [
-        currentLongitude,
-        setCurrentLongitude
-      ] = useState(Number);
-      const [
-        currentLatitude,
-        setCurrentLatitude
-      ] = useState(Number);
-     
-    Geolocation.getCurrentPosition(
-        //Will give you the current location
-        (position) => {
-        
-        //Setting Longitude state
-        setCurrentLongitude(position.coords.longitude);
-        
-        //Setting Longitude state
-        setCurrentLatitude(position.coords.latitude);
-            }, (error) => Alert.alert(error.message), { 
-            enableHighAccuracy: false,timeout :200 ,maximumAge: 1000 
-            //if we enableHighaccuracy the time of response might be longer than false & increased power consumption
-            }
-        );
-
-    return (
+    const [position, setPosition] = useState({
+        latitude: 10,
+        longitude: 10,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.001,
+      });
+    
+      useEffect(() => {
+        Geolocation.getCurrentPosition((pos) => {
+          var crd = pos.coords;
+          setPosition({
+            latitude: crd.latitude,
+            longitude: crd.longitude,
+            latitudeDelta: 0.0421,
+            longitudeDelta: 0.0421,
+          });
+        })
+      }, []);
+    
+      return (
         <View>
-            <Text>Position: Long:{currentLongitude} Lat:{currentLatitude} </Text>
+            <Text>Longitudine: {position.longitude}; Latitudine:{position.latitude}</Text>
         </View>
-    );
-
-}
+      );
+    };
+     
 export default Location;
