a4fa4c6ae9690f1e3c4bb5794ceb0c917e9468c0
minosse99 <simoneboldrini1999@gmail.com>
2022-10-16 12:15:31 +0200

    update location with real-time-render

--- app/src/components/Location.tsx
+++ app/src/components/Location.tsx
@@ -1,35 +1,63 @@
-import React, {useState,useEffect} from 'react';
-import { View, Text} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Geolocation from '@react-native-community/geolocation'
 
+interface IProps{}
+interface IState {
+  latitude: number,
+  longitude: number,
+  error:string
+}
+class Location extends Component<IProps,IState> {
+  watchId : any;
 
-import Geolocation from '@react-native-community/geolocation';
+  constructor(props: any) {
+    super(props);
 
-
-const Location = () => {
-    const [position, setPosition] = useState({
-        latitude: 10,
-        longitude: 10,
-        latitudeDelta: 0.001,
-        longitudeDelta: 0.001,
-      });
-    
-      useEffect(() => {
-        Geolocation.getCurrentPosition((pos) => {
-          var crd = pos.coords;
-          setPosition({
-            latitude: crd.latitude,
-            longitude: crd.longitude,
-            latitudeDelta: 0.0421,
-            longitudeDelta: 0.0421,
-          });
-        })
-      }, []);
-    
-      return (
-        <View>
-            <Text>Longitudine: {position.longitude}; Latitudine:{position.latitude}</Text>
-        </View>
-      );
+   this.state= {
+      latitude: 0,
+      longitude: 0,
+      error:""
     };
-     
+
+   setInterval(this.location.bind(this),3000)
+  }
+
+ location(){
+    this.watchId = Geolocation.getCurrentPosition(
+      (position) => {
+        let obj = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: ""
+        }
+        this.setState(obj);
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 2000, maximumAge : 1000},
+    );
+  }
+
+ componentDidMount() {
+    this.location()
+  }
+
+ componentWillUnmount() {
+    Geolocation.clearWatch(this.watchId);
+  }
+
+ render() {
+    return (
+
+             <View style={{ position: 'relative', top: 70, flexGrow: 1, 
+alignItems: 'center', justifyContent: 'center'} }>
+              <Text style={{color:'red'}}> Latitude: {this.state.latitude}</Text>
+              <Text style={{color:'red'}}>Longitude: {this.state.longitude}</Text>
+                {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+             </View>
+
+    );
+  }
+}
+
 export default Location;
