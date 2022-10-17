17380059c6e1d66bd12f38b4ed09f76c2cfe4502
Lucajett99 <lucagenova99@gmail.com>
2022-10-17 21:04:52 +0200

    added geojson

--- app/src/components/Map.tsx
+++ app/src/components/Map.tsx
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet, Alert } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
 import MapboxGL from "@rnmapbox/maps";
 import Geolocation from '@react-native-community/geolocation'
+import { zone } from '../support/utils';
 
 const ACCESS_TOKEN: string = "sk.eyJ1IjoibHVjYWpldHQiLCJhIjoiY2w5YnBxYjZlMGIyejNwbzAxaDgyanh1dSJ9.o9IxOf7DSZpBPirPkoTUFQ"
+
 
 MapboxGL.setWellKnownTileServer('Mapbox');
 MapboxGL.setAccessToken(ACCESS_TOKEN);
 
 interface IProps{}
 interface IState {
-    coordinates: [number, number ] , req: boolean
+    coordinates: [number, number],
+    req: boolean
 }
 class Map extends Component<IProps,IState> {
     constructor(props: any) {
@@ -20,25 +23,24 @@ class Map extends Component<IProps,IState> {
           coordinates: [11.346597927619127, 44.49422570257762], //coordinate base Bologna
           req : false
         };
-        this.location();
-       setInterval(this.location.bind(this),3000)
+        //setInterval(this.getCurrentLocation.bind(this),3000)
     }
 
-    location(){
+    private getCurrentLocation = async () => {
       Geolocation.getCurrentPosition(
         (position) => {
-          let arr: [number,number] = [position.coords.longitude,position.coords.latitude]
-          let obj ={ coordinates : arr , req: true};
+          const location: [number, number] = [position.coords.longitude,position.coords.latitude]
+          let obj = { coordinates : location , req: true};
           this.setState(obj);
         },
-        (error) => {Alert.alert("Error in getCurrentPosition")},
+        (error) => {Alert.alert("Error in getCurrentLocation()")},
         { enableHighAccuracy: (this.state.req ? false : true), timeout: 3000, maximumAge : 10000},
       );
     }
 
     componentDidMount() {
       MapboxGL.setTelemetryEnabled(false);
-      this.location();
+      this.getCurrentLocation();
     }
   
     render() {
@@ -50,6 +52,17 @@ class Map extends Component<IProps,IState> {
               zoomLevel={this.state.req ? 15 : 6}
               centerCoordinate={this.state.coordinates}
             />
+             <MapboxGL.ShapeSource id={'some-feature'} shape={zone}>
+                <MapboxGL.LineLayer
+                    sourceID="some-feature"
+                    id="some-feature-line"
+                    style={{
+                        lineColor: 'red',
+                        lineWidth: 3,
+                    }}
+                />
+                <MapboxGL.FillLayer id="fill" style={{ fillColor: "#b9f2ff", fillOpacity: 0.7  }} />
+            </MapboxGL.ShapeSource>
             <MapboxGL.PointAnnotation coordinate={this.state.coordinates} />
           </MapboxGL.MapView>
         </View>
