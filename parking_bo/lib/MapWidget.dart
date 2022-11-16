ff3962495ab656a42b02ec42688b3db5c04d953d
MarcoTomasone <tomasone35@gmail.com>
2022-11-16 13:16:05 +0100

    [ADD] Plotting map polygons from GeoJson

--- parking_bo/lib/MapWidget.dart
+++ parking_bo/lib/MapWidget.dart
@@ -4,35 +4,66 @@ import 'package:flutter_map/flutter_map.dart'; // Suitable for most situations
 import 'package:flutter_map/plugin_api.dart'; // Only import if required functionality is not exposed by default
 import 'package:latlong2/latlong.dart';
 
-class MapWidget extends StatelessWidget{
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart'; 
+import 'package:geojson/geojson.dart';
+import 'package:flutter/services.dart' show rootBundle;
+
+
+
+
+class MapWidget extends StatefulWidget{ 
   var mapCenter = new LatLng(44.493754, 11.343095);
+  var vertex1 = new LatLng(44.500000, 11.343095);
+  var vertex3 = new LatLng(44.483754, 11.363295);
+ 
+  @override
+  _MapWidgetState createState() => _MapWidgetState();
+  }
+
+class _MapWidgetState extends State<MapWidget>{
+  
+  /// Data for the Flutter map polylines layer
+  final polygons = <Polygon>[];
+
+  Future<void> parseAndDrawAssetsOnMap() async {
+    final geo = GeoJson();
+    geo.processedPolygons.listen((GeoJsonPolygon polygon) {
+      /// when a line is parsed add it to the map right away
+      List<LatLng> myPoints = <LatLng>[];
+      polygon.geoSeries.forEach((element) {
+        myPoints = element.toLatLng();
+      });
+      setState(() => polygons.add(Polygon(points: myPoints, color: Colors.lightBlue.shade50.withOpacity(0.6), isFilled: true)));
+      
+    });
+    geo.endSignal.listen((_) => geo.dispose());
+    final data = await rootBundle.loadString('assets/zone.geojson');
+    final features = await geo.parse(data, verbose:true);
+  }
 
   @override
+  void initState() {
+        parseAndDrawAssetsOnMap();
+  }
+  @override
   Widget build(BuildContext context) {
-   return FlutterMap(
+    return FlutterMap(
       options: MapOptions(
-          center: mapCenter,
+          center: widget.mapCenter,
           zoom: 13.5,
       ),
       children: [
-                TileLayer(
-                 urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-                userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+              TileLayer(
+                  urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                  userAgentPackageName: 'dev.fleaflet.flutter_map.example',
                ),
-
               PolygonLayer(
                 polygonCulling: false,
-                polygons: [
-                      Polygon(
-                        points: [LatLng(30, 40), LatLng(20, 50), LatLng(25, 45),],
-                        color: Colors.blue,
-                      ),
-                ],
-              )
-            ] 
+                polygons : polygons
+              ),
+            ],
         );
-    
   }
-
-
 }
+
