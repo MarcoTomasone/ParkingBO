f1899a47499d9b607a51ddfc6095c1debd2a8fb0
minosse99 <simoneboldrini1999@gmail.com>
2022-12-06 11:47:13 +0100

    add sendTransition

--- parking_bo/lib/MapWidget.dart
+++ parking_bo/lib/MapWidget.dart
@@ -1,18 +1,20 @@
+import 'package:ParkingBO/utils/httpRequest.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart'; // Suitable for most situations
 import 'package:flutter_map/plugin_api.dart'; // Only import if required functionality is not exposed by default
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
+import './utils/newTypes.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:geojson/geojson.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'ActivityRecognitionClass.dart';
-import 'package:flutter_activity_recognition/flutter_activity_recognition.dart' as ar;
+import 'package:flutter_activity_recognition/flutter_activity_recognition.dart'
+    as ar;
 import 'package:fluttertoast/fluttertoast.dart';
-
 
 final LocationSettings locationSettings = LocationSettings(
   accuracy: LocationAccuracy.high,
@@ -21,21 +23,44 @@ final LocationSettings locationSettings = LocationSettings(
 
 class MapWidget extends StatefulWidget {
   //Initialize Activity Recognition class
- 
+
   @override
   _MapWidgetState createState() => _MapWidgetState();
 }
 
-class _MapWidgetState extends State<MapWidget> { 
+class _MapWidgetState extends State<MapWidget> {
   /// Data for the Flutter map polylines layer
   final polygons = <Polygon>[];
-  ar.ActivityType currentActivity = ar.ActivityType.UNKNOWN; 
+  ar.ActivityType currentActivity = ar.ActivityType.UNKNOWN;
   LatLng location = LatLng(44.493754, 11.343095); //Coordinates of Bologna
   late CenterOnLocationUpdate _centerOnLocationUpdate;
-  late ActivityRecognition activityRecognition; //= new ActivityRecognition(updateCurrentActivity);
+  late ActivityRecognition
+      activityRecognition; //= new ActivityRecognition(updateCurrentActivity);
 
-
-  void updateCurrentActivity(ar.ActivityType activityType){
+  void updateCurrentActivity(ar.ActivityType activityType) {
+    if (currentActivity == ar.ActivityType.IN_VEHICLE &&
+        activityType == ar.ActivityType.WALKING) {
+      Fluttertoast.showToast(
+          msg: "Change from driving to walking",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      sendTransition(ParkingType.EXITING,location);
+    } else if (currentActivity == ar.ActivityType.WALKING &&
+        activityType == ar.ActivityType.IN_VEHICLE) {
+      Fluttertoast.showToast(
+          msg: "Change from walking to driving",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      sendTransition(ParkingType.ENTERING,location);
+    }
     setState(() {
       currentActivity = activityType;
     });
@@ -46,10 +71,8 @@ class _MapWidgetState extends State<MapWidget> {
         timeInSecForIosWeb: 3,
         backgroundColor: Colors.red,
         textColor: Colors.white,
-        fontSize: 16.0
-    );
+        fontSize: 16.0);
   }
-
 
   Future<void> drawPolygonsOnMap() async {
     final geo = GeoJson();
@@ -69,13 +92,8 @@ class _MapWidgetState extends State<MapWidget> {
     final features = await geo.parse(data, verbose: true);
   }
 
-
- 
-
-
-
   @override
-  void initState(){
+  void initState() {
     //_getPermissionLocation();
     activityRecognition = new ActivityRecognition(updateCurrentActivity);
     drawPolygonsOnMap();
@@ -86,8 +104,9 @@ class _MapWidgetState extends State<MapWidget> {
         location = LatLng(position.latitude, position.longitude);
       });
     });
-  }
 
+    sendTransition(ParkingType.ENTERING,location);
+  }
 
   @override
   dispose() {
@@ -95,16 +114,14 @@ class _MapWidgetState extends State<MapWidget> {
     activityRecognition.dispose();
   }
 
-
-  IconData getMarkerType(){
-    if(currentActivity == ar.ActivityType.IN_VEHICLE)
+  IconData getMarkerType() {
+    if (currentActivity == ar.ActivityType.IN_VEHICLE)
       return Icons.navigation; // CASE: DRIVING
-    else if(currentActivity == ar.ActivityType.WALKING)
+    else if (currentActivity == ar.ActivityType.WALKING)
       return Icons.circle; // CASE: Walking
-    else 
+    else
       return Icons.my_location; //CASE: Still, Unknown
-    }
-
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -128,12 +145,14 @@ class _MapWidgetState extends State<MapWidget> {
         PolygonLayer(polygonCulling: false, polygons: polygons),
         CurrentLocationLayer(
           centerOnLocationUpdate: _centerOnLocationUpdate,
-          ),
+        ),
         MarkerLayer(markers: [
-          Marker(point: location, builder: (ctx) => Icon(
-            getMarkerType(),          
-            color: Colors.red,)
-          ),
+          Marker(
+              point: location,
+              builder: (ctx) => Icon(
+                    getMarkerType(),
+                    color: Colors.red,
+                  )),
         ])
       ], //Children
     );
--- parking_bo/lib/utils/httpRequest.dart
+++ parking_bo/lib/utils/httpRequest.dart
@@ -1,26 +1,26 @@
 import 'dart:developer' as dev;
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:latlong2/latlong.dart';
 import 'newTypes.dart';
 import 'package:flutter_activity_recognition/flutter_activity_recognition.dart';
 
 //Per fare localhost su real mobile ci va l'ip del computer su cui fate girare il server
 //Per fare localhost sull'emulatore dovrebbe andare il seguente ip = 10.0.2.2
-const url = 'http://192.168.1.67:8000';
+const url = 'http://192.168.45.34:8000';
 
 /**
  * This function is used to send to the server the users's activity
  */
-Future<http.Response> sendTransition(ParkingType type) async {
-  final request = {'type': type.toString(), 'position': 'position.toJson()'};
-  final response = await http.post(
-    Uri.parse('${url}/sendActivity'),
-    body:  json.encode(request),
-    headers: {
-      "Content-Type": "application/json",
-      "Accept" : "application/json"
-    }
-  );
+Future<http.Response> sendTransition(ParkingType type, LatLng position) async {
+  final request = {'type': type.toString(), 'position': position.toJson()};
+  final response = await http.post(Uri.parse('${url}/sendTransition'),
+      body: json.encode(request),
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      });
   if (response.statusCode == 200) {
     // If the server did return a 200 CREATED response,
     dev.log("The user's transition was sent successfully");
@@ -30,3 +30,4 @@ Future<http.Response> sendTransition(ParkingType type) async {
   };
   return response;
 }
+
