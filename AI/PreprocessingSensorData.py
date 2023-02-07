cdbf9a2ecf8c738950d6653ef2a522c9abbb76c3
minosse99 <simoneboldrini1999@gmail.com>
2023-02-07 12:07:36 +0100

    Sensor data set in dataframe

--- AI/PreprocessingSensorData.py
+++ AI/PreprocessingSensorData.py
@@ -1,7 +1,10 @@
 import json
 import pandas as pd
 
-f = open('./supports/SensorData.json')
+from itertools import repeat
+from operator import itemgetter
+
+f = open('./supports/SensorData1.json')
 data = json.load(f)
 f.close()
 
@@ -15,8 +18,25 @@ accelerometerList.rename(columns={'x':'accelerometer_x','y':'accelerometer_y','z
 uAccList.rename(columns={'x':'uAccelerometer_x','y':'uAccelerometer_y','z':'uAccelerometer_z'},inplace=True)
 gyroscopeList.rename(columns={'x':'gyroscope_x','y':'gyroscope_y','z':'gyroscope_z'},inplace=True)
 magnetometerList.rename(columns={'x':'magnetometer_x','y':'magnetometer_y','z':'magnetometer_z'},inplace=True)
+#target =data.get('target');
 
-data = pd.concat([accelerometerList,uAccList,gyroscopeList,magnetometerList],axis=1)
-##Manca aggiungere TARGET (e ovviamente duplicare le tuple per avere un numero pari per ogni classe)
+target = pd.json_normalize(data,record_path=['target']);
+
+
+#print(accelerometerList.join(target.iloc(1),how='left')
+df = pd.DataFrame(data = {'expected': [],'detected':[]})
+for i in range(0,len(data)):
+    targetElem = target['expected'][i]
+    #print(targetElem)
+    exp = tuple(repeat(targetElem, len(data[i].get('accelerometerList'))))
+    dec = tuple(repeat(target['detected'][i], len(data[i].get('accelerometerList'))))
+    df = df.append(pd.DataFrame(data = {'expected': exp,'detected':dec}),ignore_index=True)
+    
+
+    
+df.reset_index(drop=True, inplace=True)
+data = pd.concat([accelerometerList,uAccList,gyroscopeList,magnetometerList,df],axis=1)
+
+#data.to_excel('./SensorData.xlsx',index=False)
 print(data)
 
--- parking_bo/lib/utils/SensorsClass.dart
+++ parking_bo/lib/utils/SensorsClass.dart
@@ -3,7 +3,7 @@ class SensorClass {
     List<Map<String, double>> gyroscopeList;
     List<Map<String, double>> magnetometerList;
     List<Map<String, double>> uAccelerometerList;
-    List<String> target;
+    List<Map<String, String>> target;
 
     SensorClass(this.accelerometerList, this.gyroscopeList, this.magnetometerList, this.uAccelerometerList, this.target);
 
