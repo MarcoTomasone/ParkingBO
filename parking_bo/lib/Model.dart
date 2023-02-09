0f6fa0234b0a856f5d65461d66958b91336d3746
minosse99 <simoneboldrini1999@gmail.com>
2023-02-09 14:14:48 +0100

    Merge MapWidget file

---
+++ parking_bo/lib/Model.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:sklite/SVM/SVM.dart';
+import 'package:sklite/ensemble/forest.dart';
+import 'package:sklite/utils/io.dart';
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'dart:developer' as dev;
+class Model {
+  RandomForestClassifier? model;
+
+  void loadModel() async {
+    await rootBundle.loadString("assets/test1.json").then((value) {
+      this.model = RandomForestClassifier.fromMap(json.decode(value));
+    });
+  }
+
+  predict(List<double> data) {
+    return this.model!.predict(data);
+  }
+}
