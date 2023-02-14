e10652888285bc793cde2ec55da34ac512de06ef
MarcoTomasone <tomasone35@gmail.com>
2023-02-14 11:58:11 +0100

    [FIX] Fixed load widget on user not giving permissions

--- AI/RFSensorData.py
+++ AI/RFSensorData.py
@@ -6,27 +6,29 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix,ConfusionMatrixDisplay
-from sklite import LazyExport
 import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
 
-realdata = pd.read_csv('./supports/real_data.csv')
-data =pd.read_csv('./supports/har_dataset_preprocessedUnscaledWithStill.csv')
+
+real_dataDF = pd.read_csv('./supports/real_data.csv')
+
+har_dataDF =pd.read_csv('./supports/har_dataset_preprocessedUnscaledWithStill.csv')
 
 #  Creiamo un modello addestrato sia con i dati reali che con quelli di HAR 
 # e poi lo testiamo per vedere se riesce a riconoscere i movimenti reali 
+#divide both inputs in train and test sets
+har_dataDF = har_dataDF.reindex(columns=['android.sensor.accelerometer#mean','android.sensor.accelerometer#max','android.sensor.accelerometer#min','android.sensor.accelerometer#std','android.sensor.gyroscope#mean','android.sensor.gyroscope#max','android.sensor.gyroscope#min','android.sensor.gyroscope#std','android.sensor.gyroscope_uncalibrated#mean','android.sensor.gyroscope_uncalibrated#max','android.sensor.gyroscope_uncalibrated#min','android.sensor.gyroscope_uncalibrated#std','target'])
+har_dataDFTrain, har_dataDFTest = train_test_split(har_dataDF, test_size=0.1, random_state=42, shuffle=True)
+real_dataDFTrain, real_dataDFTest = train_test_split(real_dataDF, test_size=0.1,random_state=42,shuffle=True)
+#create the full training set and shuffle it
+dataset = pd.concat([har_dataDFTrain, real_dataDFTrain])
+dataset = shuffle(dataset, random_state=42)
 
-data = data.reindex(columns=['android.sensor.accelerometer#mean','android.sensor.accelerometer#max','android.sensor.accelerometer#min','android.sensor.accelerometer#std','android.sensor.gyroscope#mean','android.sensor.gyroscope#max','android.sensor.gyroscope#min','android.sensor.gyroscope#std','android.sensor.gyroscope_uncalibrated#mean','android.sensor.gyroscope_uncalibrated#max','android.sensor.gyroscope_uncalibrated#min','android.sensor.gyroscope_uncalibrated#std','target'])
-y = data['target'] 
-x = data.drop(['target'],axis=1)
-
-yr = realdata['target']
-xr = realdata.drop(['target'],axis=1)
-X_trainR, X_testR,y_trainR, y_testR = train_test_split(xr, yr, test_size=0.1,random_state=42,shuffle=True)
-
+y = dataset['target'] 
+x = dataset.drop(['target'],axis=1) 
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.1,random_state=42,shuffle=True)
+
 rf = RandomForestClassifier(n_estimators=300)
-X_train = pd.concat([X_trainR,X_train])
-y_train = pd.concat([y_trainR,y_train])
 rf.fit(X_train, y_train)
 
 y_pred = rf.predict(X_test)
@@ -34,10 +36,24 @@ y_pred = rf.predict(X_test)
 print('Accuracy:', metrics.accuracy_score(y_test, y_pred))
 print('Precision:', metrics.confusion_matrix(y_test, y_pred))
 
-y_predR = rf.predict(X_testR)
-print('Accuracy:', metrics.accuracy_score(y_testR, y_predR))
-print('Precision:', metrics.confusion_matrix(y_testR, y_predR))
-cf = metrics.confusion_matrix(y_testR, y_predR,labels=rf.classes_)
+#Once fitted model, prepare two test set for real and har data
+
+y_har_test = har_dataDFTest['target'] 
+x_har_test = har_dataDFTest.drop(['target'],axis=1)
+
+
+y_real_test = real_dataDFTest['target']
+x_real_test = real_dataDFTest.drop(['target'],axis=1)
+y_predp = rf.predict(x_har_test)
+
+print('AccuracyProf:', metrics.accuracy_score(y_har_test, y_predp))
+print('Precision:', metrics.confusion_matrix(y_har_test, y_predp))
+
+
+y_predR = rf.predict(x_real_test)
+print('Accuracy Real:', metrics.accuracy_score(y_real_test, y_predR))
+print('Precision:', metrics.confusion_matrix(y_real_test, y_predR))
+cf = metrics.confusion_matrix(y_real_test, y_predR,labels=rf.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix = cf,display_labels=rf.classes_)
 disp.plot()
 plt.show()
